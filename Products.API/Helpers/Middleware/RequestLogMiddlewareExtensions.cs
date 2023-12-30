7baa1a36609988be3619ca7cb4c0883c0c4c4a42
Victor Lopez <stevelopezc@gmail.com>
2023-12-30 00:26:57 -0600

    Adding Middleware to Log request time

---
+++ Products.API/Helpers/Middleware/RequestLogMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Products.API.Helpers.Middleware
+{
+    public static class RequestLogMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLogMiddleware>();
+        }
+    }
+}
